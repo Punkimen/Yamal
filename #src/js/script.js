30176b68079179bbf558565de2449f4e255b51e6
max <punkimen666@gmail.com>
2021-09-24 13:31:19 +0300

    Yamal

---
+++ #src/js/script.js
@@ -0,0 +1,283 @@
+"use strict";
+@@include('./_aos.js');
+document.addEventListener("DOMContentLoaded", function () {
+    // slide-menu
+    (function () {
+        const searchInputElem = $('.location-menu__search-inp');
+        $('.header__burger-menu').on('click', () => {
+            $('.header-menu').slideToggle();
+        });
+        $('.header__location').on('click', () => {
+            $('.location-menu--main').slideToggle();
+        });
+        $('.fixed-menu__header-location').on('click', () => {
+            $('.location-menu--fixed').slideToggle();
+        });
+        $('.js-show-cities').on('click', () => {
+            $('.location-menu__lists').slideToggle();
+        });
+        $('.js-hide-location').on('click', () => {
+            $('.location-menu__lists').hide();
+            $('.location-menu').hide();
+        });
+        $('.location-menu__category-btn').on('click', function () {
+            searchInputElem.val($(this).text())
+        });
+        $('.select-language').on('click', () => {
+            if ($('.current-lang').text() === 'ENG') {
+                $('.current-lang').text('RU')
+            } else {
+                $('.current-lang').text('ENG')
+            }
+        })
+        const setElementForMobile = () => {
+            const windowWidth = $(window).width();
+            if (windowWidth <= 767) {
+                $('.header__center').append($('.header__logo'));
+                $('.header__right').append($('.header__burger-menu'));
+            } else {
+                $('.header__center').append($('.header__burger-menu'));
+                $('.header__left').prepend($('.header__logo'));
+            }
+        }
+        setElementForMobile()
+        $(window).resize(() => {
+            setElementForMobile();
+        })
+    })();
+    // sliders
+    (function () {
+        const weatherSlider = new Swiper('.northern-lights__slider', {
+            slidesPerView: 1,
+        })
+        const swiperCalendar = new Swiper('.playbill-events__calendar-slider', {
+            slidesPerView: "auto",
+            freeMode: true,
+        })
+        const swiperEvent = new Swiper('.playbill-events__slider--event', {
+            slidesPerView: "auto",
+            spaceBetween: 20,
+        })
+        const swiperBanners = new Swiper('.playbill-events__slider--banner', {
+            slidesPerView: "auto",
+            spaceBetween: 20,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            breakpoints: {
+                568: {
+                    spaceBetween: 20,
+                    pagination: false,
+                }
+            }
+        });
+        const swiperGoods = new Swiper('.goods-wrapper', {
+            slidesPerView: "auto",
+            spaceBetween: 20,
+            breakpoints: {
+                568: {
+                    slidesPerView: 4,
+                }
+            }
+        })
+
+        const swiperGallery = new Swiper('.gallery__slider', {
+            slidesPerView: "auto",
+            spaceBetween: 20,
+            createElements: true,
+            scrollbar: {
+                el: '.swiper-scrollbar',
+            },
+            breakpoints: {
+                767: {
+                    slidesPerView: 4,
+                }
+            }
+        })
+        $('.calendar-arrow__next').on('click', () => {
+            swiperCalendar.slideNext();
+        })
+        $('.event-arrow__next').on('click', () => {
+            swiperEvent.slideNext();
+        })
+        $('.banner-arrow__next').on('click', () => {
+            swiperBanners.slideNext();
+        })
+        $('.goods-arrow__next').on('click', () => {
+            swiperGoods.slideNext();
+        })
+        $('.gallery-arrow__next').on('click', () => {
+            swiperGallery.slideNext();
+        })
+        $('.gallery-arrow__prev').on('click', () => {
+            swiperGallery.slidePrev();
+        })
+        $('.northern-lights-arrow__prev').on('click', () => {
+            weatherSlider.slidePrev();
+        })
+        $('.northern-lights-arrow__next').on('click', () => {
+            weatherSlider.slideNext();
+        })
+    })();
+    // adaptive hide
+    (function () {
+        const adaptivePosBlock = () => {
+            const windowWidth = $(window).width();
+            const parentWidth = $('.quiz-individual').width();
+            const childWidth = $('.quiz-individual__wrapper').width();
+            let posBlock = (childWidth - parentWidth) / 3;
+            if (posBlock > 0 && windowWidth > 568) {
+                console.log('w');
+
+                $('.quiz-individual__wrapper').css({
+                    "right": `${posBlock}px`
+                })
+            }
+        }
+        adaptivePosBlock();
+        $(window).resize(() => {
+            adaptivePosBlock();
+        })
+    })();
+    // playbill
+    (function () {
+        $('.playbill-category').on('click', function () {
+            $(this).addClass('current').siblings().removeClass('current')
+        })
+    })();
+    // fixed-menu
+    (function () {
+        $(window).on('scroll', function () {
+            if ($(this).scrollTop() >= $(this).height()) {
+                $('.fixed-menu').addClass('show-leftMenu')
+            } else {
+                $('.fixed-menu').removeClass('show-leftMenu')
+            }
+        })
+        $('.fixed-menu').on('mouseenter', function () {
+            $(this).addClass('show')
+        });
+        $('.fixed-menu').on('mouseleave', function () {
+            $(this).removeClass('show')
+        });
+        $('.fixed-menu__close').on('click', () => {
+            $('.fixed-menu').removeClass('show');
+            $('.fixed-menu__menu-block').removeClass('active');
+        })
+        $('.js-show-menu').on('click', (e) => {
+            $('.js-show-menu').toggleClass('current')
+            $('.fixed-menu').toggleClass('menu-isOpen')
+            $('.fixed-menu').removeClass('show')
+            $('.fixed-menu__menu-block').toggleClass('active');
+        });
+    })();
+    // parallax
+    (function () {
+        AOS.init({
+            easing: 'ease-out-back',
+        });
+        var rellax = new Rellax('.rellax', {
+            center: true,
+        });
+    })();
+    // scroll
+    (function () {
+        $('.playbill__categoryes').mCustomScrollbar({
+            axis: "x",
+        }); $('.photos__wrapper--scroll').mCustomScrollbar({
+            axis: "x",
+        });
+    })();
+    // select-city
+    (function () {
+        $('.custom-select__header').on('click', function () {
+            $(this).toggleClass('active')
+            $(this).next().slideToggle();
+            $('.custom-select__categoryes').mCustomScrollbar({
+                axis: "y",
+            });
+            $('.custom-select__category__elem .control').on('click', function () {
+                let text = $(this).text()
+                $('.custom-select__current-elem').text(text)
+                $('.custom-select__header').removeClass('active');
+                $('.custom-select__body').hide();
+            })
+        })
+    })();
+    // quiz
+    (function () {
+        let count = 0;
+        const anketa = [
+            {
+                question: "Культура и история",
+                answer: ['Не интересует', 'Могу уделить немного времени', 'Очень интересует', 'Хочу полностью погрузиться']
+            },
+            {
+                question: "Природа и приключения",
+                answer: ['Стараюсь избегать', 'Только популярные места', 'Очень интересует', 'Я - авантюрист']
+            },
+            {
+                question: "Культура и история",
+                answer: ['Не люблю делать покупки', 'Если будет свободное время', 'Остановлюсь на магнитиках', 'Хочу купить подарки']
+            },
+            {
+                question: "Культура и история",
+                answer: ['Не хочу пробовать новые блюда', 'Не против экспериментов', 'Присоединюсь к мастер-классам', 'Хочу погрузиться в местную кухню']
+            },
+        ];
+        const renderQuestions = (count) => {
+            const quizInnerBlock = document.querySelector('.quiz__questions-block');
+            quizInnerBlock.innerHTML = `
+            <div class="questions-block__title">${anketa[count].question}</div>
+                <div class="questions-block__wrapper">
+                    <button type="button" class="questions-block__answer btn" data-value="${anketa[count].answer[0]}">
+                    <svg class="">
+                    <use xlink:href="./img/icons/icons.svg#emoji-happy"></use>
+                </svg>
+                        <span>${anketa[count].answer[0]}</span>
+                    </button>
+                    <button type="button" class="questions-block__answer btn" data-value="${anketa[count].answer[1]}">
+                    <svg class="">
+                    <use xlink:href="./img/icons/icons.svg#emoji-normal"></use>
+                </svg>
+                        <span>${anketa[count].answer[1]}</span>
+                    </button>
+                    <button type="button" class="questions-block__answer btn" data-value="${anketa[count].answer[2]}">
+                    <svg class="">
+                    <use xlink:href="./img/icons/icons.svg#emoji-normal-1"></use>
+                </svg>
+                        <span>${anketa[count].answer[2]}</span>
+                    </button>
+                    <button type="button" class="questions-block__answer btn" data-value="${anketa[count].answer[3]}">
+                    <svg class="">
+                    <use xlink:href="./img/icons/icons.svg#emoji-medium"></use>
+                </svg>
+                        <span>${anketa[count].answer[3]}</span>
+                    </button>
+                </div>
+                <div class="questions-block__dott"></div>
+            `
+            $('.questions-block__answer').on('click', function () {
+                $('.quiz-routes__progress-item').eq(count).addClass('past')
+                if (count != 3) {
+                    count++;
+                    renderQuestions(count)
+                } else if (count === 3) {
+                    $('.quiz-routes__progress-item').eq(4).addClass('active')
+                }
+
+                $('.quiz-routes__progress-item').eq(count).addClass('active')
+            })
+        }
+        renderQuestions(count)
+        $('.btn-return').on('click', function () {
+            count = $(this).attr('data-count')
+            const parent = $(this).parents('.quiz-routes__progress-item')
+            parent.nextAll().removeClass('active past')
+            renderQuestions(count)
+        })
+    })();
+})
+
+
